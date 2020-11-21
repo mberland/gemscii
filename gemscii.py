9bd3fc1468c19dbf0dc63f631793e1835d285eaf
M Berland <mberland@wisc.edu>
2020-11-20 20:35:05 -0600

    typing and errors and some color

--- gemscii.py
+++ gemscii.py
@@ -5,12 +5,10 @@
 # . . X X . # # % X X # .
 # % . % % X * % X # * % #
 # * X . % # * * # X . X X
+import random
+from typing import Tuple, List
 
 import tcod
-import random
-
-
-from collections import defaultdict
 
 gems = "ABCDE"
 MATRIX_WIDTH = 10
@@ -30,14 +28,11 @@ def new_gem():
 
 
 def init_matrix():
-    return [[random.choice(gems) for j in range(MATRIX_HEIGHT)] for i in range(MATRIX_WIDTH)]
+    return [[random.choice(gems) for _ in range(MATRIX_HEIGHT)] for _ in range(MATRIX_WIDTH)]
 
 
 def meta_matrix():
     return {i: {j: {} for j in range(MATRIX_HEIGHT)} for i in range(MATRIX_WIDTH)}
-
-
-# print(meta_matrix()[width - 1][height - 1])
 
 
 def elt(m, x, y):
@@ -55,33 +50,30 @@ def display(c="|", i=-1, j=-1):
         print("{:5s}".format(c), end="")
 
 
-def print_matrix(m):
-    new_line = lambda : display()
-    for j in range(MATRIX_HEIGHT):
-        new_line()
-        new_line()
-        for i in range(MATRIX_WIDTH):
-            display(c=elt(m, i, j), i=i, j=j)
-    new_line()
+# def print_matrix(m):
+#     new_line = lambda: display()
+#     for j in range(MATRIX_HEIGHT):
+#         new_line()
+#         new_line()
+#         for i in range(MATRIX_WIDTH):
+#             display(c=elt(m, i, j), i=i, j=j)
+#     new_line()
 
 
-def print_meta(m,meta,streaks):
-    assert False, "UNIMPLEMENTED"
+# def print_meta(m, meta, streaks):
+#     assert False, "UNIMPLEMENTED"
 
 
-def cell_options(m):
+def matrix_streaks(m: List) -> List:
     streaks = set()
     deltas = [(i, j) for i in range(-1, 2) for j in range(-1, 2) if i != 0 or j != 0]
     cells = [(i, j) for j in range(MATRIX_HEIGHT) for i in range(MATRIX_WIDTH)]
     for cell in cells:
         for delta in deltas:
             wins = 0
-            cx = cell[0]
-            cy = cell[1]
-            dx = delta[0]
-            dy = delta[1]
-            ax = dx
-            ay = dy
+            cx, cy = cell
+            dx, dy = delta
+            ax, ay = delta
             cell_type = elt(m, cx, cy)
             streak = {tuple([cell_type, cx, cy])}
             while cell_type and valid_x(cx + ax) and valid_y(cy + ay) and cell_type == elt(m, cx + ax, cy + ay):
@@ -93,72 +85,77 @@ def cell_options(m):
                 new_streak = frozenset(streak)
                 if not any(new_streak.issubset(s) for s in streaks):
                     streaks.add(frozenset(streak))
-                # output_matrix[elt_id(cx,cy)] = cell_type + str(cx)+str(cy)
-    streaks = [x for x in streaks if not any(x.issubset(s) for s in streaks if s != x)]
-    #
-    # for s in streaks:
-    #     print(sorted(s))
-    return streaks
+    return [x for x in streaks if not any(x.issubset(s) for s in streaks if s != x)]
 
 
-def print_streaks(m, sx):
-    for streak in sx:
-        for cell in sorted(streak, key=lambda e: e[1]):
-            x = cell[1]
-            y = cell[2]
-            m[x][y] = elt(m, x, y) + "!"
-    print_matrix(m)
-
-#
-# matrix = init_matrix()
-# meta = meta_matrix()
-# # print_matrix(matrix)
-# streaks = cell_options(matrix)
-# print_streaks(matrix, streaks)
+# def print_streaks(m, sx):
+#     for streak in sx:
+#         for cell in sorted(streak, key=lambda e: e[1]):
+#             _, x, y = cell
+#             m[x][y] = elt(m, x, y) + "!"
+#     print_matrix(m)
 
 
-def fill_matrix(m: object) -> object:
-    for i in range(MATRIX_WIDTH):
-        for j in range(MATRIX_HEIGHT):
-            if m[i][j] == "#":
-                if j < (MATRIX_HEIGHT - 1):
-                    m[i][j] = m[i][j+1]
-                    m[i][j+1] = new_gem()
-                else:
-                    m[i][j] = new_gem()
-    return m
+def matrix_fill(m: List) -> object:
+    mm = meta_matrix()
+    for _ in range(MATRIX_HEIGHT):
+        for i in range(MATRIX_WIDTH):
+            for j in range(MATRIX_HEIGHT):
+                if m[i][j] == "#":
+                    mm[i][j]["color"] = "GREEN"
+                    if j < (MATRIX_HEIGHT - 1):
+                        m[i][j] = m[i][j + 1]
+                        m[i][j + 1] = new_gem()
+                    else:
+                        m[i][j] = new_gem()
+    return m, mm
 
 
-def update_from_streak(m: object, streak: object) -> object:
+def update_from_streak(m: List, mm: object, streak: List) -> object:
     for cell in streak:
-        cx = cell[1]
-        cy = cell[2]
+        _, cx, cy = cell
         m[cx][cy] = "#"
-    return m
+    return m, mm
 
 
-def update_matrix(m: object) -> object:
-    streaks = cell_options(m)
+def matrix_update(m: List, mm: object) -> object:
+    streaks = matrix_streaks(m)
     if len(streaks) > 0:
         relevant_streak = random.choice(streaks)
-        return update_from_streak(m,relevant_streak)
+        return update_from_streak(m, mm, relevant_streak)
+    return m, mm
+
+
+color_map = dict({"RED": ((255, 255, 255), (255, 0, 0)), "GREEN": ((255, 255, 255), (0, 255, 0)), "WHITE": ((255, 255, 255), (0, 0, 0))})
+
+
+def char_colors(x, y, m, mm) -> Tuple[Tuple[int, int, int], Tuple[int, int, int]]:
+    meta_info = mm[x][y]
+    if "color" in meta_info:
+        return color_map[meta_info["color"]]
     else:
-        return m
+        c = m[x][y]
+        if "#" == c:
+            return color_map["RED"]
+    return color_map["WHITE"]
 
 
 WINDOW_WIDTH = MATRIX_WIDTH * 4
-WINDOW_HEIGHT = MATRIX_HEIGHT * 4 #= 40, 24 # Console width and height in tiles.
+WINDOW_HEIGHT = MATRIX_HEIGHT * 4
+X_BUFFER: int = int(WINDOW_WIDTH / (1 + MATRIX_WIDTH))
+Y_BUFFER: int = int(WINDOW_HEIGHT / (1 + MATRIX_HEIGHT))
+X_START: int = 2 * X_BUFFER
+Y_START: int = Y_BUFFER
 
 
-def tcod_matrix(console, m):
-    BUFFER_X: int = int(WINDOW_WIDTH / (1 + MATRIX_WIDTH))
-    BUFFER_Y: int = int(WINDOW_HEIGHT / (1 + MATRIX_HEIGHT))
+def matrix_tcod(console, m: List, mm: object):
     for j in range(MATRIX_HEIGHT):
         for i in range(MATRIX_WIDTH):
-            cx = 2 * BUFFER_X + i * BUFFER_X
-            cy = BUFFER_Y + j * BUFFER_Y
+            cx = X_START + i * X_BUFFER
+            cy = Y_START + j * Y_BUFFER
             ce = m[i][j]
-            console.print(x=cx, y=cy, string=ce)
+            fg, bg = char_colors(i, j, m, mm)
+            console.print_box(x=cx, y=cy, string=ce, fg=fg, bg=bg, width=1, height=1)
 
 
 def main() -> None:
@@ -167,28 +164,32 @@ def main() -> None:
     console = tcod.Console(WINDOW_WIDTH, WINDOW_HEIGHT)
     matrix = init_matrix()
     meta = meta_matrix()
-    print_matrix(matrix)
-    
+    # print_matrix(matrix)
+
     with tcod.context.new(columns=console.width, rows=console.height, tileset=tileset) as context:
         while True:  # Main loop, runs until SystemExit is raised.
             console.clear()
-            # tcod_matrix(console,matrix)
-            # # console.print(x=0, y=0, string="Hello World!")
-            # context.present(console)  # Show the console.
+            matrix_tcod(console, matrix, meta)
+            context.present(console)
 
             for event in tcod.event.wait():
                 # context.convert_event(event)  # Sets tile coordinates for mouse events.
                 if event.type == "KEYDOWN":
-                    # print_matrix(matrix)
                     # print(event)
                     console.clear()
-                    if event.sym == tcod.event.K_d:
-                        matrix = update_matrix(matrix)
-                    if event.sym == tcod.event.K_a:
-                        matrix = fill_matrix(matrix)
-                    if event.sym == tcod.event.K_r:
+                    if event.sym == tcod.event.K_q:
+                        raise SystemExit()
+                    elif event.sym == tcod.event.K_d:
+                        matrix, meta = matrix_update(matrix, meta)
+                    elif event.sym == tcod.event.K_s:
+                        streaks = matrix_streaks(matrix)
+                    elif event.sym == tcod.event.K_a:
+                        matrix, meta = matrix_fill(matrix)
+                    elif event.sym == tcod.event.K_r:
                         matrix = init_matrix()
-                    tcod_matrix(console,matrix)
+                        meta = meta_matrix()
+
+                    matrix_tcod(console, matrix, meta)
                     context.present(console)  # Show the console.
                 if event.type == "QUIT":
                     raise SystemExit()
